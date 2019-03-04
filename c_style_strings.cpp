228f26cc614d6c558e92d04c70e572e0589e3506
Jerry Yang <15632602+heureuxchaquejour@users.noreply.github.com>
2019-03-04 18:50:24 +0800

    Update c_style_strings.cpp

--- c_style_strings.cpp
+++ c_style_strings.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 
 int main() {
-	const char* string = "Yang";
-	char* string2 = (char*)"Yoong"; 
+	const char string[] = {'Y', 97, 'n', 'g', 0};
+	char* string2 = (char*)"Yoong";
 	// MS Compiler would yell at me without (char*),
 	// because "Yoong" is by default a const char [6]
-	std::cout << string2 << std::endl;
+	std::cout << string2 <<std::endl;
 	std::cin.get();
 
 }
